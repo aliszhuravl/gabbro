ab4e0cedb947c1129d9ea0bc4bad10ecfb53208a
aliszhuravl <alis.zhuravl@gmail.com>
2026-02-23 15:06:07 +0300

    23v2

--- web/js/main.js
+++ web/js/main.js
@@ -588,12 +588,3 @@ $("#show-more").click(function(){
     $(".smth-else").fadeIn( 500 ).css('display', 'flex').removeClass('hidden');
     $("#show-more").addClass('hidden');
 });
-
-var $input = $('input.tel');
-if(!mobilecheck()){
-    //для ПК
-    $input.attr('type', 'text').mask("(999) 999-9999");;
-} else {
-    //для телефона
-    $input.attr('type', 'number'); //или 'tel'
-}
